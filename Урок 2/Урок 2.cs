9694d04843547c099975c0e9263caee9bbee657b
Katrin85 <Em-1985@yandex.ru>
2022-08-15 21:11:07 +0300

    калькулятор case

--- Урок 2/Урок 2.cs
+++ Урок 2/Урок 2.cs
@@ -1,43 +1,47 @@
-﻿Console.WriteLine("Ввeдите первое число: : ");
-double one = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine("Ввeдите второе число: : ");
-double two = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine( "Выбирете операцию: '+', '-', '*', '/' ");
-string? action = Console.ReadLine();
-
-switch (action)
+﻿while (true)
 {
-    case "+":
-        {
-            Console.Write("Ответ =  ");
-            Console.Write(one + two);
-        }
-        break;
-    case "-":
-        {
-            Console.Write("Ответ =  ");
-            Console.Write(one - two);
-        }
-        break;
-    case "*":
-        {
-            Console.Write("Ответ =  ");
-            Console.Write(one * two);
-        }
-        break;
-    case "/":
-        if (two == 0)
-            Console.WriteLine("На ноль делить НЕЛЬЗЯ!");
-        else
-        {
-            Console.Write("Ответ =  ");
-            Console.Write(one / two);
-        }
-        break;
-    default:
-        Console.WriteLine("Я не знаю такого действия, попробуйте еще раз");
-        break;
+    Console.Clear();
+    Console.WriteLine("Ввeдите первое число: : ");
+    double one = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Ввeдите второе число: : ");
+    double two = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Выбирете операцию: '+', '-', '*', '/' ");
+    string? action = Console.ReadLine();
+
+    switch (action)
+    {
+        case "+":
+            {
+                Console.Write("Ответ =  ");
+                Console.Write(one + two);
+            }
+            break;
+        case "-":
+            {
+                Console.Write("Ответ =  ");
+                Console.Write(one - two);
+            }
+            break;
+        case "*":
+            {
+                Console.Write("Ответ =  ");
+                Console.Write(one * two);
+            }
+            break;
+        case "/":
+            if (two == 0)
+                Console.WriteLine("На ноль делить НЕЛЬЗЯ!");
+            else
+            {
+                Console.Write("Ответ =  ");
+                Console.Write(one / two);
+            }
+            break;
+        default:
+            Console.WriteLine("Я не знаю такого действия, попробуйте еще раз");
+            break;
+    }
+    Console.ReadLine();
 }
-Console.ReadLine();
