15d82ccb3448a55e90d81381eb35458442eb7006
Katrin85 <Em-1985@yandex.ru>
2022-08-16 13:56:18 +0300

    количество четных и нечетных числ, и считает их сумму

--- Урок 3/Program.cs
+++ Урок 3/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Программа будет подсчитывать количество четных и нечетных числ, и считает их сумму в заданном пользователем диапозоне.
+
+uint evenNumbers = 0; // четные числа
+uint oddNumbers = 0;  // нечетне числа
+int sumEven = 0;
+int sumOdd = 0;
+
+
+Console.WriteLine("Введите первое число диапозона");
+int current = int.Parse(Console.ReadLine()); // начальное число, от которого будем считать
+
+Console.WriteLine("Введите последнее число диапозона");
+int limit = int.Parse(Console.ReadLine());  // до какого числа будем подсчитывать
+while (current <= limit)
+{
+    if (current % 2 == 0)
+    {
+        evenNumbers++;
+        sumEven += current;
+    }
+    else
+    {
+        oddNumbers++;
+        sumOdd = sumOdd + current;
+    }
+    current++;
+}
+
+
+
+Console.WriteLine("Количество четных чисел: " + oddNumbers);
+Console.WriteLine("Сумма четных чисел в диапозоне = " + sumEven);
+Console.WriteLine("Количество нечетных чисел " + evenNumbers);
+Console.WriteLine("Сумма нечетных чисел в диапозоне = " + sumOdd);
+Console.ReadLine();
