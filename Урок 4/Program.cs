c2b2b8f37b1d2e284c7db21e5b4796995a71c524
Katrin85 <Em-1985@yandex.ru>
2022-08-17 12:31:37 +0300

    add

---
+++ Урок 4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Урок_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            for ( ; i < 3; i++)
+            {
+                Console.WriteLine("for_1:" + i);
+            }
+            for (; i < 5; i++)
+            {
+                Console.WriteLine("for_2:" + i);
+            }
+        }
+    }
+}
