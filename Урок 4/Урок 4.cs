2afe6ba1a2fba2cd980722f79978ccfabe9559ac
Katrin85 <Em-1985@yandex.ru>
2022-08-19 19:54:52 +0300

    add

---
+++ Урок 4/Урок 4.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Урок_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Урок 4
+
+            //    int i = 0;
+            //    for (; i < 3; i++)
+            //    {
+            //        Console.WriteLine("for_1:" + i);
+            //    }
+            //    for (; i < 5; i++)
+            //    {
+            //        Console.WriteLine("for_2:" + i);
+            //    }
+
+            //for (int i = 0, j = 5; i < 10 && j < 12; i++, j++)
+            //{
+            //    Console.WriteLine("i: " + i);
+            //    Console.WriteLine("j:" + j);
+            //}
+
+            //int i = int.Parse(Console.ReadLine());
+
+            //for (; i >= 0; i--)  // цикл for в обратную сторону
+            //{
+            //  Console.WriteLine(i);
+            //}
+
+            int i = int.Parse(Console.ReadLine());
+            int j = int.Parse(Console.ReadLine());
+            for (; i >= 0; i--, j++)                        // цикл for в обратную сторону
+            {
+                Console.WriteLine(i);
+                Console.WriteLine(j);
+            }
+        }
+    }
+}
