ed619482dccb5fa2c1559ca9ff4ab4f97fd2970c
Katrin85 <Em-1985@yandex.ru>
2022-08-20 22:00:29 +0300

    Цикла в цикле

---
+++ Урок 7.1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Урок_7._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите высоту прямоугольника: ");
+            int height = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите ширину прямоугольника: ");
+            int Widht = int.Parse(Console.ReadLine());
+
+            for (int j = 0; j < height; j++)
+            {
+                for (int i = 0; i < Widht; i++)
+                
+                    Console.Write("+");
+                
+             Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Урок 7/Урок 7.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Урок_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+            // Вложенные циклы
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Цикл 1 итерация №: " + i);
+
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.WriteLine("\tЦикл 2 итерация №: " + j);
+
+                    for (int k = 0; k <= 2; k++)
+                        Console.WriteLine("\t\tЦикл 3 итерация №: " + k);
+                }
+            }
+        }
+    }
+}
