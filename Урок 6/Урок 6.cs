d21a9ace973a6c97dbd5e8b3409ba4b952e26b58
Katrin85 <Em-1985@yandex.ru>
2022-08-20 10:15:50 +0300

    continue

---
+++ Урок 6/Урок 6.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Урок_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // continue
+            for (int i = 0; i < 5; i++)
+            {
+                if (i == 2)
+                continue;
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
