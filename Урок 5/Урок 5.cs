5d24db691d194024181830d1bb04d2dfc9a7bd0d
Katrin85 <Em-1985@yandex.ru>
2022-08-20 09:58:16 +0300

    break

---
+++ Урок 5/Урок 5.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Урок_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // break
+
+            for (int i = 0; i < 100; i++)
+            { 
+                string msg = Console.ReadLine();
+
+                if (msg == "exit")
+                { 
+                    break;
+                }
+
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+            
+        }
+    }
+}
