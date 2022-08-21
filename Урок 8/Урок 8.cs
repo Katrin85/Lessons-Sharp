f8e015015d4d29d05ff47b2d99a868c5536d42c5
Katrin85 <Em-1985@yandex.ru>
2022-08-21 12:51:42 +0300

    тернарный оператор

---
+++ Урок 8/Урок 8.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Урок_8
+{
+    internal class Program
+    {  // тернарный оператор или замена if_else
+        static void Main(string[] args)
+        {
+            bool accessAllowed;
+            string storedPassword = "qwerty";
+            string enteredPassword = Console.ReadLine();
+
+            //if (enteredPassword == storedPassword)
+            //{
+            //    accessAllowed = true;
+            //}
+            //else
+            //{
+            //    accessAllowed = false;
+            //}
+            //Console.WriteLine(accessAllowed);
+
+            //Console.ReadLine();
+
+            accessAllowed = enteredPassword == storedPassword?  true: false;
+               
+            Console.WriteLine(accessAllowed);
+        }
+    }
+}
