54bfdb36464fa91f4d118b686e4cb9e1e8333e91
Katrin85 <Em-1985@yandex.ru>
2022-08-21 14:54:02 +0300

    вывод элементов массива на консоль в обратном порядке

--- Урок 11/Урок 11.cs
+++ Урок 11/Урок 11.cs
@@ -10,6 +10,24 @@ namespace Урок_11
     {
         static void Main(string[] args)
         {
+            Console.Write("Введите количество элементов массива:\t ");
+            int elementsCount = int.Parse(Console.ReadLine());
+            int[] myArray = new int[elementsCount];
+
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                Console.WriteLine($"\nВведите элемент массива под индексом {i}:\t");
+                myArray[i] = int.Parse(Console.ReadLine());
+
+            }
+
+            Console.WriteLine("\nВывод массива: ");
+            for (int i = myArray.Length - 1; i >= 0; i--)  // вывод элементов массива на консоль в обратном порядке
+            {
+                Console.WriteLine(myArray[i]);
+            }
+
+
         }
     }
 }
