d07084946c3e4d424b32333196e3090dd9b63e95
Katrin85 <Em-1985@yandex.ru>
2022-08-21 18:04:55 +0300

    Наименьшее число в массиве

---
+++ Урок 13/Урок 13.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Урок_13
+{ // Наименьшее число в массиве
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = { 2, 3, 99, 49, 64, 77, 4, 42, 5 };
+            int minValue = myArray[0];
+
+            for (int i = 1; i < myArray.Length; i++)
+            {
+                if (myArray[i] < minValue)
+                    minValue = myArray[i];
+            }
+            Console.WriteLine(minValue);
+        }
+    }
+}
